216a5d9b8b16db1a6adc803ebbd440c2113075ff
NgocPhuong-98 <66169022+NgocPhuong-98@users.noreply.github.com>
2020-06-30 15:00:24 +0700

    PTW1
    PTW1

--- 21/21.php
+++ 21/21.php
@@ -7,7 +7,7 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 
-    require_once "libs/scss.inc.php";
+    require_once 'libs/scss.inc.php';
 
     use ScssPhp\ScssPhp\Compiler;
 
@@ -15,9 +15,9 @@ $url_path = str_replace('\\', '/', $url_path);
 
     $scss->setImportPaths('sass/');
 
-    $styles = $scss->compile('@import "1740-styles.scss";');
+    $styles = $scss->compile('@import "21-styles.scss";');
 
-    $css = fopen('css/1740-styles.css','w');
+    $css = fopen('css/21-styles.css','w');
     
     fwrite($css,$styles);
 ?>
@@ -27,26 +27,24 @@ $url_path = str_replace('\\', '/', $url_path);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="<?php echo $url_path ?>/css/21-styles.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo $url_path ?>/css/1740-styles.css" rel="stylesheet" type="text/css" />
-    
     <link href="<?php echo $url_path ?>/fontawesome-free-5.13.0-web/css/all.min.css" rel="stylesheet" />
     <link href="<?php echo $url_path ?>/swiper-5.4.1/package/css/swiper.min.css" rel="stylesheet" />
     <link href="<?php echo $url_path ?>/family-font/Roboto-Black.ttf" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="<?php echo $url_path ?>/swiper-5.4.1/package/js/swiper.min.js"></script>
     <script src="<?php echo $url_path ?>/js/jquery-3.5.1.min.js"></script>
-    <script src="js/1740.js"></script>
-
-
+    <script  src="<?php echo $url_path ?>/js/scripts.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/21.js"></script>
+    <title>Document</title>
 
 </head>
 
 <body>
     <?php
-     include "./1740-content.php";
+     include "./21-content.php";
    ?>
 </body>
-
-
 </html>
